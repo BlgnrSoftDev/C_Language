e1fb73c56c9e2952fd0040b813e9a07922db0d41
huseyin <19360859036@ogrenci.btu.edu.tr>
2020-12-05 04:12:06 +0300

    yeni projeler eklendi

---
+++ Ornek_75/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ayristirici(int a,int b,char c[]) ;
+int main(){
+
+char dizi[5][20]={"Benim",
+                  "Adim",
+                  "Huseyin",
+                  "Bilginer",
+                  "ulan"};
+
+
+char aranacakkelime[20] ;
+int kontrol,sondeger,calisti = 0;
+
+printf("Lutfen arayacaginiz kelimeyi giriniz :\t");
+gets(aranacakkelime) ;
+
+for(int i = 0;i < 5;i++){
+    kontrol = 0 ;
+    for(int j = 0;j < strlen(aranacakkelime);j++){
+        if(aranacakkelime[j] == dizi[i][j] ){
+            kontrol++;
+            sondeger = j ;
+        }
+        else{
+            break ;
+        }
+        }
+    if(kontrol == strlen(aranacakkelime)){
+        calisti = 1 ;
+        ayristirici(i,sondeger,aranacakkelime) ;
+        break ;
+    }
+    }
+
+if(calisti == 0)
+        printf("aradiginiz kelime bulunamamistir.... :( ") ;
+
+
+ imza();
+    return 0 ;
+}
+
+
+void ayristirici(int a,int b,char c[]){
+printf("Aradiginiz kelime bulunmustur!! olleyy\n\n") ;
+printf("Sirasiyla aradiginiz kelimenin harflerinin konumlari :\n") ;
+for(int m = 0;m < b+1;m++){
+    printf("%c . harf dizinin %d satirinin %d. sutunundadir. \n",c[m],a+1,m+1) ;
+}
+}
+
+
+void imza (void){
+printf("\n\n\n--------------------------------------> BlgnrSoftDev was here  ^_^ <--------------- ") ;
+}
---
+++ Ornek_76/main.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int dizi[6] = {45, 87, 26, 97, 48, 11};
+    int aranan_dizi[3] = {48, 11, 0};
+
+
+    int i,j;
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(dizi[i] == aranan_dizi[j])
+            {
+                i++;
+                if(i>=6)
+                {
+                    break;
+                }
+                continue;
+            }
+            else
+            {
+                break;
+            }
+
+        }
+
+        if(j==3)
+        {
+            printf("Aranan dizi bulundu!");
+            break;
+        }
+    }
+
+    if(j!=3)
+    {
+        printf("Aranan dizi bulunamadi!");
+    }
+
+
+    return 0;
+}
